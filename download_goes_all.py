d2e29efa264f363b9cd84c7a64c509538c596b8a
Ludving Cano Fernandez <66972062+LudCano@users.noreply.github.com>
2025-01-29 09:51:20 -0400

    Update download_goes_all.py

--- download_goes_all.py
+++ download_goes_all.py
@@ -46,8 +46,8 @@ import h5netcdf
 today = dt.datetime.now()
 today = today.date().strftime('%Y-%m-%d') 
 print('TODAY IS' ,today)
-date0 = '2023-02-01'
-datef = '2023-02-14'
+date0 = '2023-02-15'
+datef = '2023-02-28'
 
 ######################################################
 #######   CONEXIÓN A AMAZON WEB SERVICES   ###########
